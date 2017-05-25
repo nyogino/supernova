0551867130837b78252e076f811bda0e8c854846
nyogino <nyogino@gmail.com>
2017-05-25 17:57:09 +0900

    modified by nyogino

--- main.py
+++ main.py
@@ -25,23 +25,25 @@ def board_dict():
     board = api.board(product_code=product_code)
     return(board["mid_price"], board["asks"], board["bids"])
 
+def gap_calc(*args, **kwargs):
+    count = args[0]
+    ask_list = bid_list = []
+
+    for ask in asks[0:count]:
+        ask_list.append(ask["size"] * ask["price"])
+    for bid in bids[0:count]:
+        bid_list.append(bid["size"] * bid["price"])
+
+    print("ask gap from mid", abs(mid - sum(ask_list)/2))
+    print("bid gap from mid", abs(mid - sum(bid_list)/2))
+
 
 """ MAIN """
 
 
 mid, asks, bids = board_dict()
 
-
-# for bid in bids[0:5]:
-#     print(bid)
-
-print("--")
 print(mid)
 print("--")
 
-ask_list = []
-for ask in asks[0:2]:
-    ask_list.append(ask["size"] * ask["price"])
-
-print("ask gap from mid", abs(mid - sum(ask_list)/2))
-
+gap_calc(2, mid=mid, asks=asks, bids=bids)
